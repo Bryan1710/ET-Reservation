778347bf05ece9a8e70ce631ce57ef7069f4ba87
Bryan1710 <bryanmarasigan1710@gmail.com>
2022-12-26 14:35:26 +0800

    E-Reservation

---
+++ app/src/main/java/com/app/projectbookstore/UserCategories.java
@@ -0,0 +1,14 @@
+package com.app.projectbookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserCategories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_categories);
+    }
+}
